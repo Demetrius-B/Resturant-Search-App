08494837cd9b19232196dd9a465ca8b4a99b57d9
demetrius-b <Dberkeley@fullsail.edu>
2019-09-13 15:09:17 -0400

    created navigator & searchScreen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const SearchScreen = () => {
+    return <View>
+        <Text>Search Screen</Text>
+    </View>
+}
+
+export default SearchScreen;
