e0ec3fbb1f2d3ae06d897a24dbb667b81437873a
demetrius-b <Dberkeley@fullsail.edu>
2019-09-16 13:50:15 -0400

    create ResultDetail to show more data on each juice

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Text, View, Image, StyleSheet } from 'react-native'
+
+const ResultsDetail = ({result}) => {
+    return (
+        <View>
+            <Text>Results Detail:</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default ResultsDetail;
