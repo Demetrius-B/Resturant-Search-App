85227f67e385cd2142b653226ef1ce1070598cb6
demetrius-b <Dberkeley@fullsail.edu>
2019-09-20 15:01:16 -0400

    added margin for all elements

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -13,14 +13,15 @@ const ResultsDetail = ({result}) => {
 
 const styles = StyleSheet.create({
     container: {
-        marginLeft: 10
+        marginLeft: 15
     },
     imageStyle: {
         width: 250,
         height: 120,
         borderRadius: 4,
         borderColor: 'black',
-        borderWidth: 2
+        borderWidth: 1,
+        marginBottom: 5
     },
     name: {
         fontWeight: 'bold'
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({title, results}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
             <FlatList
                 horizontal
@@ -18,9 +18,14 @@ const ResultsList = ({title, results}) => {
 }
 
 const styles = StyleSheet.create({
+    container:{
+        marginBottom: 10
+    },
     titleStyle: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
     }
 })
 
