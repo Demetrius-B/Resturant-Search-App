c8690bc2d6b1c94377c73cbc11b72d1be148df38
demetrius-b <Dberkeley@fullsail.edu>
2019-09-20 14:48:28 -0400

    added margin for consinatnt edges & added  borderColor

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,7 +3,7 @@ import {Text, View, Image, StyleSheet } from 'react-native'
 
 const ResultsDetail = ({result}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Image style={styles.imageStyle} source={{uri: result.img_url}} />
             <Text style={styles.name}>{result.name}</Text>
             <Text>${result.price}, {result.stockCount} left in stock.</Text>
@@ -12,14 +12,19 @@ const ResultsDetail = ({result}) => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        marginLeft: 10
+    },
     imageStyle: {
         width: 250,
         height: 120,
-        borderRadius: 4
+        borderRadius: 4,
+        borderColor: 'black',
+        borderWidth: 2
     },
     name: {
         fontWeight: 'bold'
     }
 })
 
-export default ResultsDetail;
+export default ResultsDetail
